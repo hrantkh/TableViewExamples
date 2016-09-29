0415f78886579574225fa9119feb320c067963d7
Seryozha Poghosyan <seryozhapoghosyan@workfront.com>
2016-09-29 21:00:55 +0400

    Created FirstViewController.

--- TableViewExamples/FirstViewController.h
+++ TableViewExamples/FirstViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  FirstViewController.h
 //  TableViewExamples
 //
 //  Created by Seryozha Poghosyan on 9/29/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface FirstViewController : UIViewController
 
 @end
-
---
+++ TableViewExamples/FirstViewController.m
@@ -0,0 +1,22 @@
+//
+//  FirstViewController.m
+//  TableViewExamples
+//
+//  Created by Seryozha Poghosyan on 9/29/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
