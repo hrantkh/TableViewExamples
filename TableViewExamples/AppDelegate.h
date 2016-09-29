a5e641b5454c83c4ec8439080b078cac37938a41
Seryozha Poghosyan <seryozhapoghosyan@workfront.com>
2016-09-29 20:42:45 +0400

    Created project

---
+++ TableViewExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewExamples
+//
+//  Created by Seryozha Poghosyan on 9/29/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewExamples
+//
+//  Created by Seryozha Poghosyan on 9/29/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
